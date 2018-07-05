3fb8df63904643e616f9d2f8d2bf7f69af3446ad
kondo takeshi <takeshi.kondo@quipper.com>
2018-07-05 22:18:39 +0900

    add s3 backend

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.11.0"
+
+  backend "s3" {
+    bucket  = "terraform-state-backet"
+    key     = "terraform.tfstate.aws"
+    profile = "hello-aws"
+    region  = "ap-northeast-1"
+  }
+}
+
 variable "region" {
     default = "ap-northeast-1"
 }
