efe513f2ab3668ebcae49853476dc9d97c77445e
kondo takeshi <takeshi.kondo@quipper.com>
2018-07-06 00:59:56 +0900

    add credential vars

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ terraform {
   }
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "region" {
     default = "ap-northeast-1"
 }
@@ -28,6 +30,8 @@ variable "images" {
 }
  
 provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
     region = "${var.region}"
 }
  
