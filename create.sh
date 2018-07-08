7a7ee355f0dfba041f55fa8b73046fdee37bb903
kondo takeshi <takeshi.kondo@quipper.com>
2018-07-08 19:22:42 +0900

    wait for ssh port opened

--- create.sh
+++ create.sh
@@ -2,4 +2,16 @@
 terraform apply -auto-approve
 nginx_ip=$(terraform output | awk '{print $3}')
 echo "[web]\n${nginx_ip}" > hosts
+
+echo "---waiting for ssh port opened---"
+while true
+do
+  nc -z -w 1 $nginx_ip 22
+  if [ $? = 0 ]; then
+    break
+  else
+    echo "---not open, retry---"
+    sleep 3
+  fi
+done
 ansible-playbook -i hosts site.yml
