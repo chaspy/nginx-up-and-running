1f606016136a5de75869fb99f726baabc9bb3cdb
kondo takeshi <takeshi.kondo@quipper.com>
2018-07-08 19:26:43 +0900

    start nginx

--- main.tf
+++ main.tf
@@ -79,6 +79,12 @@ resource "aws_security_group" "admin" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
